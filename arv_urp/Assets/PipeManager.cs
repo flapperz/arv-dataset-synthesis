a67c8d699f7b9fda9c9ecb58a5c0a9c670906bcf
Krit Cholapand <flap.atyz@gmail.com>
2021-12-13 07:15:24 +0700

    Init repo + unity setup

---
+++ arv_urp/Assets/PipeManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeManager : MonoBehaviour
+{
+    public GameObject FlangeNode;
+
+    public GameObject ANode;
+
+    public GameObject Corner;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
